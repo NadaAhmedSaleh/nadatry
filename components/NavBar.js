25a1fd8f5066b121e75fa1605a80d33d087f0eb3
NadaAhmedSaleh <nada.ahmedsaleh98@gmail.com>
2021-07-08 17:38:46 +0200

    img to Image

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component, useState,useEffect,useContext } from 'react';
 import { AppStateContext } from "../AppGlobalState";
 import Link from "next/link"
+import Image from 'next/image'
 function NavBar(props) {
 
  
@@ -22,7 +23,7 @@ function NavBar(props) {
   
     return (
         <div className="navBar-container"   >
-            <img alt="Logo" className="navBar-logo" src='https://s3-eu-west-1.amazonaws.com/wuzzuf/files/company_logo/Gebhaly-com-Egypt-32768-1593097553.png' />
+            <Image alt="Logo"  src='https://s3-eu-west-1.amazonaws.com/wuzzuf/files/company_logo/Gebhaly-com-Egypt-32768-1593097553.png' height="60px" width="200px"/>
             <span style={{display:"flex",alignItems:"center"}}>
             <div className="navBar-link"> <Link  href="/">Products </Link></div>
             <div className="navBar-link"> <Link  href="/checkoutpage">Cart </Link></div>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['static.wixstatic.com','images.squarespace-cdn.com','www.iconpacks.net','image.flaticon.com','cdn.pixabay.com','upload.wikimedia.org'],
+    domains: ['s3-eu-west-1.amazonaws.com','static.wixstatic.com','images.squarespace-cdn.com','www.iconpacks.net','image.flaticon.com','cdn.pixabay.com','upload.wikimedia.org'],
   },
 }
